12b9a732d781690f99ea4284b11a1c0b8356367d
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-09-15 11:16:42 +0100

    Update write_bibliography.r

--- inst/chunks/write_bibliography.r
+++ inst/chunks/write_bibliography.r
@@ -10,16 +10,16 @@ sessionInfo()$otherPkgs %>%
  base::subset(!grepl('helpr|projectData|datarepository|project.notes.tools', .)) %>%
  knitr::write_bib(file='files/software.bib')
 
-RefManageR::GetBibEntryWithDOI(doi=software_doi) %>%
-  when(!is.null(.)~RefManageR::WriteBib(bib=., file='files/software.bib', append=TRUE)) %>%
-  invisible()
+if(length(software_doi)>0)
+  RefManageR::GetBibEntryWithDOI(doi=software_doi) %>%
+    RefManageR::WriteBib(bib=., file='files/software.bib', append=TRUE)
 
 sprintf(fmt='cd %s ; academic import --bibtex %s/files/software.bib --publication-dir publication/software --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
 
 #! write academic references
 
-RefManageR::GetBibEntryWithDOI(doi=academic_doi) %>%
-  when(!is.null(.)~RefManageR::WriteBib(bib=., file='files/academic.bib', append=FALSE)) %>%
-  invisible()
+if(length(academic_doi)>1)
+  RefManageR::GetBibEntryWithDOI(doi=academic_doi) %>%
+    RefManageR::WriteBib(bib=., file='files/academic.bib', append=FALSE)
 
 sprintf(fmt='cd %s ; academic import --bibtex %s/files/academic.bib --publication-dir publication/academic --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
