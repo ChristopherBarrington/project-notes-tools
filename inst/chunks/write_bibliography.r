45663b1d76b8592dbf4679242fab0230238a3995
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-09-28 17:29:03 +0100

    change to single bibliography

--- inst/chunks/write_bibliography.r
+++ inst/chunks/write_bibliography.r
@@ -1,25 +1,34 @@
 
 #! write software/packages references
 
-sessionInfo()$otherPkgs %>%
- sapply(purrr::pluck, 'Package') %>%
- base::subset(!grepl('helpr|projectData|datarepository|project.notes.tools', .)) %>%
- knitr::write_bib(file='files/software.bib') %>%
- invisible()
-
-if(length(software_doi)>0)
-  RefManageR::GetBibEntryWithDOI(doi=software_doi) %>%
-    RefManageR::WriteBib(bib=., file='files/software.bib', append=TRUE)
-
-file.rename(from='files/software.bib', to='files/software.bibtex') %>% invisible()
-sprintf(fmt='cd %s ; academic import --bibtex %s/files/software.bibtex --publication-dir publication/software --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
+# sessionInfo()$otherPkgs %>%
+#  sapply(purrr::pluck, 'Package') %>%
+#  base::subset(!grepl('helpr|projectData|datarepository|project.notes.tools', .)) %>%
+#  knitr::write_bib(file='files/software.bib') %>%
+#  invisible()
+#
+# if(length(software_doi)>0)
+#   RefManageR::GetBibEntryWithDOI(doi=software_doi) %>%
+#     RefManageR::WriteBib(bib=., file='files/software.bib', append=TRUE)
+#
+# file.rename(from='files/software.bib', to='files/software.bibtex') %>% invisible()
+# sprintf(fmt='cd %s ; academic import --bibtex %s/files/software.bibtex --publication-dir publication/software --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
 
 #! write academic references
 
-if(length(academic_doi)>0) {
-  RefManageR::GetBibEntryWithDOI(doi=academic_doi) %>%
-    RefManageR::WriteBib(bib=., file='files/academic.bib', append=FALSE)
+# if(length(academic_doi)>0) {
+#   RefManageR::GetBibEntryWithDOI(doi=academic_doi) %>%
+#     RefManageR::WriteBib(bib=., file='files/academic.bib', append=FALSE)
+#
+#   file.rename(from='files/academic.bib', to='files/academic.bibtex') %>% invisible()
+#   sprintf(fmt='cd %s ; academic import --bibtex %s/files/academic.bibtex --publication-dir publication/academic --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
+# }
 
-  file.rename(from='files/academic.bib', to='files/academic.bibtex') %>% invisible()
-  sprintf(fmt='cd %s ; academic import --bibtex %s/files/academic.bibtex --publication-dir publication/academic --no-overwrite', website_path, knitting_path) %>% system(ignore.stdout=TRUE)
+#! write bibliography
+if(length(bibliography_doi)>0) {
+  RefManageR::GetBibEntryWithDOI(doi=bibliography_doi) %>%
+    RefManageR::WriteBib(bib=., file='scratch/bibliography.bib', append=FALSE)
+
+  sprintf(fmt='cd %s ; academic import --bibtex %s/scratch/bibliography.bib --publication-dir publication --no-overwrite', website_path, knitting_path) %>%
+    system(ignore.stdout=TRUE)
 }
