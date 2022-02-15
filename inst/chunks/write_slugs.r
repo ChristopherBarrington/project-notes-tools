7d9b0f2365657c092dc761b73585431c77c82b39
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-02-15 16:37:17 +0000

    Update write_slugs.r

--- inst/chunks/write_slugs.r
+++ inst/chunks/write_slugs.r
@@ -1,13 +1,13 @@
 ```{r write_slugs}
 #| cache=FALSE,
 #| echo=FALSE,
+#| eval=opts_current$get('slugs') %>% is.null() %>% not(),
 #| results='asis'
 
 slugs <- opts_current$get('slugs')
 
-if(is.null(slugs) %>% not())
-  Map(slug=slugs, name=names(slugs), function(slug, name)
-      str_replace_all(name, '_', ' ') %>% sprintf(fmt='[%s]: {{< ref "%s" >}}', slug)) %>%
-    str_c(collapse='\n') %>%
-    cat()
+Map(slug=slugs, name=names(slugs), function(slug, name)
+    str_replace_all(name, '_', ' ') %>% sprintf(fmt='[%s]: {{< ref "%s" >}}', slug)) %>%
+  str_c(collapse='\n') %>%
+  cat()
 ```
