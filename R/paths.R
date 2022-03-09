7254f9f7ea965c14628f20fbcaba2281cba6bbfa
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-03-09 14:05:12 +0000

    Update paths.R

--- R/paths.R
+++ R/paths.R
@@ -1,4 +1,17 @@
-
+#' Paths for the project
+#' 
+#' A list of functions that provide paths to different parts of the project.
+#' 
+#' @param ... Ignored.
+#' 
+#' @value
+#' Each function returns a character string absolute path.
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_remove
+#' 
+#' @export
+#' 
 list(slug=function(...) getwd() %>% basename(),
      knitting_path=function(...) getwd(),
      content_path=function(...) getwd() %>% file.path(., 'content'),
@@ -6,4 +19,4 @@ list(slug=function(...) getwd() %>% basename(),
      project_path=function(...) system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
      scientist_path=function(...) system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
      lab_path=function(...) system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
-     projects_path=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> paths
+     projects_path=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
