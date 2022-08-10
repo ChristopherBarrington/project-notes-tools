cd0fa32185c4e7b89e3214c65f325542e1a35cb6
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-08-10 10:28:17 +0100

    fix project_paths (revert)

--- R/paths.R
+++ R/paths.R
@@ -7,11 +7,11 @@
 #' @value
 #' Each function returns a character string absolute path.
 #' 
-list(slug=function(...) getwd() |> basename(),
-     knitting=function(...) getwd(),
-     content=function(...) getwd() |> stringr::str_remove('/content/.*') |> file.path('content'),
-     website=function(...) getwd() |> stringr::str_remove('/content/.*'),
-     project=function(...) system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
-     scientist=function(...) system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
-     lab=function(...) system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
-     projects=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
+# list(slug=function(...) getwd() |> basename(),
+#      knitting=function(...) getwd(),
+#      content=function(...) getwd() |> stringr::str_remove('/content/.*') |> file.path('content'),
+#      website=function(...) getwd() |> stringr::str_remove('/content/.*'),
+#      project=function(...) system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
+#      scientist=function(...) system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
+#      lab=function(...) system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
+#      projects=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
