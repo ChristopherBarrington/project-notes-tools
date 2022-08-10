521b96f7a95d37fcb78b839384bba0195b586df8
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-08-10 10:22:00 +0100

    use project_paths as functions

--- R/paths.R
+++ R/paths.R
@@ -1,17 +1,17 @@
 #' Paths for the project
 #' 
-#' A list of paths to different parts of the project.
+#' A list of functions that provide paths to different parts of the project.
 #' 
 #' @param ... Ignored.
 #' 
 #' @value
-#' A character string absolute path.
+#' Each function returns a character string absolute path.
 #' 
-list(slug=getwd() |> basename(),
-     knitting=getwd(),
-     content=getwd() |> stringr::str_remove('/content/.*') |> file.path('content'),
-     website=getwd() |> stringr::str_remove('/content/.*'),
-     project=system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
-     scientist=system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
-     lab=system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
-     projects=system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
+list(slug=function(...) getwd() |> basename(),
+     knitting=function(...) getwd(),
+     content=function(...) getwd() |> stringr::str_remove('/content/.*') |> file.path(., 'content'),
+     website=function(...) getwd() |> stringr::str_remove('/content/.*'),
+     project=function(...) system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
+     scientist=function(...) system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
+     lab=function(...) system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
+     projects=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
