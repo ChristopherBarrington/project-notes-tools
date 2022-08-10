d54d9b5c36957bf8ba1e343c4770169039b1de8e
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-08-10 09:48:09 +0100

    Revert "Delete paths.R"
    This reverts commit a48054c05f07e148a5624707e6b59d18e3a2cef0.

---
+++ R/paths.R
@@ -0,0 +1,20 @@
+#' Paths for the project
+#' 
+#' A list of functions that provide paths to different parts of the project.
+#' 
+#' @param ... Ignored.
+#' 
+#' @value
+#' Each function returns a character string absolute path.
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_remove
+#' 
+# list(slug=function(...) getwd() %>% basename(),
+#      knitting=function(...) getwd(),
+#      content=function(...) getwd() %>% file.path(., 'content'),
+#      website=function(...) getwd() %>% str_remove('/content/.*'),
+#      project=function(...) system('pwd -P | cut -f 1-10 -d/', intern=TRUE),
+#      scientist=function(...) system('pwd -P | cut -f 1-9 -d/', intern=TRUE),
+#      lab=function(...) system('pwd -P | cut -f 1-8 -d/', intern=TRUE),
+#      projects=function(...) system('pwd -P | cut -f 1-7 -d/', intern=TRUE)) -> project_paths
