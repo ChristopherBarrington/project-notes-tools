3c0c57a9990c052d3ce485520fba65de22057814
ChristopherBarrington <christopher.barrington@crick.ac.uk>
2024-06-06 13:59:47 +0100

    Revert "Merge branch 'main' into quarto"
    This reverts commit a3c29e7e557f2b75196394ade3a65296d3118f1f, reversing
    changes made to d9d945f345c1e250be4f264820c2984511c49070.

--- inst/chunks/ggplot_theme.r
+++ inst/chunks/ggplot_theme.r
@@ -1,3 +1,9 @@
+# -----------------------------------------
+# define a generic customised ggplot theme
+# -----------------------------------------
+
+require(project.notes.tools)
+
 (theme_bw() +
  theme(text=element_text(size=10, colour='black'),
        panel.grid.minor=element_blank(),
--- inst/chunks/working_directory.sh
+++
@@ -1,1 +0,0 @@
-mkdir --parents files scratch
